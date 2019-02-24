d78fb18b1b73eac3e5773c9b4d1707548052bd8c
Shota Takeshima <>
2019-02-24 17:23:36 +0900

    ログイン部分のUI実装

---
+++ app/layouts/login_register.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app id="inspire">
+    <v-toolbar
+      color="#ee827c"
+      dark
+      fixed
+      app
+    >
+      <v-toolbar-title>Scket</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <Nuxt />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    name: 'LoginRegister'
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ app/pages/login.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-container
+    fluid
+    fill-height
+  >
+    <v-layout
+      align-center
+      justify-center
+    >
+      <v-flex
+        xs12
+        sm8
+        md4
+      >
+        <v-card class="elevation-12">
+          <v-card-text>
+            <v-form>
+              <v-text-field
+                prepend-icon="person"
+                label="メールアドレス"
+                type="text"
+              />
+              <v-text-field
+                id="password"
+                prepend-icon="lock"
+                label="パスワード"
+                type="password"
+              />
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer />
+            <v-btn color="primary">
+              ログイン
+            </v-btn>
+          </v-card-actions>
+          <v-divider light />
+          <div class="register_form">
+            <v-card-text>
+              アカウントをお持ちでない方はこちら
+            </v-card-text>
+            <v-card-actions class="justify-center">
+              <v-btn color="primary">
+                新規会員登録
+              </v-btn>
+            </v-card-actions>
+          </div>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Login',
+
+    layout: 'login_register'
+  }
+</script>
+
+<style scoped>
+  .register_form {
+    text-align: center;
+  }
+</style>
