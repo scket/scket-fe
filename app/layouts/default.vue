557256af8e87a8b9b803cd9c18436ec00855b924
Shota Takeshima <>
2019-03-03 23:45:14 +0900

    default.vueの変更

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,47 +1,17 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer
-      v-model="drawer"
-      fixed
-      app
-    >
-      <v-list dense>
-        <v-list-tile>
-          <v-list-tile-action>
-            <v-icon>home</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Home</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile>
-          <v-list-tile-content>
-            <v-list-tile-title>Login</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile>
-          <v-list-tile-content>
-            <v-list-tile-title>Logout</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar
-      color="#ee827c"
-      dark
-      fixed
-      app
-    >
-      <v-toolbar-side-icon @click.stop="drawer = !drawer" />
+    <v-toolbar color="#ee827c" dark fixed app>
       <v-toolbar-title>Scket</v-toolbar-title>
+      <v-spacer/>
+      <v-btn outline>
+        <v-icon small style="padding-right: 2px">home</v-icon>
+        MyPage
+      </v-btn>
     </v-toolbar>
     <v-content>
       <Nuxt />
     </v-content>
-    <v-footer
-      color="#ee827c"
-      app
-    />
+    <v-footer color="#ee827c" app/>
   </v-app>
 </template>
 
@@ -54,22 +24,28 @@
 </script>
 
 <style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
+  html {
+    font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+      Roboto, 'Helvetica Neue', Arial, sans-serif;
+    font-size: 16px;
+    word-spacing: 1px;
+    -ms-text-size-adjust: 100%;
+    -webkit-text-size-adjust: 100%;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    box-sizing: border-box;
+  }
 
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
-}
+  *,
+  *:before,
+  *:after {
+    box-sizing: border-box;
+    margin: 0;
+  }
+
+  .wrapper {
+    padding: 16px;
+    max-width: 700px;
+    margin: 15px auto;
+  }
 </style>
