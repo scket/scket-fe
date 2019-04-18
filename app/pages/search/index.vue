0f405d4a45d36a3292108b44ad7737b958ad3ebb
Shota Takeshima <>
2019-04-19 00:23:49 +0900

    検索結果ページの作成

---
+++ app/pages/search/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <br>
+    <div>検索結果画面</div>
+    <br>
+    <div>locateId: {{ this.$route.query.locateId }}</div>
+    <br>
+    <div>date: {{ this.$route.query.date }}</div>
+    <br>
+    <div>keyword: {{ this.$route.query.keyword }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData: ({query}) => {
+    // ここでクエリを受け取ってAPIを叩いて検索結果を受け取る
+    console.log(query);
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ app/store/search.js
@@ -0,0 +1,63 @@
+// 検索クエリの状態管理
+/*
+ selected: {
+    city: [],
+    date: []
+  },
+ keyword: ''
+ */
+
+export const state = () => ({
+  selected: {
+    city: [],
+    date: []
+  },
+  keyword: ''
+});
+
+export const getters = {
+  selectedCity: (state) => {
+    return state.selected.city.join('&');
+  },
+
+  selectedDate: (state) => {
+    return state.selected.date.join('&');
+  },
+
+  keyword: (state) => {
+    return state.keyword;
+  }
+};
+
+export const mutations = {
+  setSelectedCity (state, city) {
+    state.selected.city = city;
+  },
+
+  setSelectedDate (state, date) {
+    state.selected.date = date;
+  },
+
+  setKeyword (state, keyword) {
+    state.keyword = keyword
+  }
+};
+
+export const actions = {
+  setSelectedCity ({commit}, {city}) {
+    commit('setSelectedCity', city)
+  },
+
+  resetSelectedCity ({commit}) {
+    commit('setSelectedCity', [])
+  },
+
+  setSelectedDate ({commit}, {date}) {
+    commit('setSelectedDate', date)
+  },
+
+  setKeyword ({commit}, {keyword}) {
+    console.log(keyword)
+    commit('setKeyword', keyword)
+  }
+}
